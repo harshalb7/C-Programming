970d12b5265b35cd7539e6cdef9171feb1920fc6
Harshal <59523135+harshalb7@users.noreply.github.com>
2020-08-16 14:06:42 +0530

    Update studentInfo.cpp

--- studentInfo.cpp
+++ studentInfo.cpp
@@ -2,14 +2,18 @@
 #include<string.h>
 #include <cstring>
 using namespace std;
+
+//declare class student info
 class studentInfo
 {
-	int rollNo,std;
+	// declaretion of variables
+	int rollNo,std;   
 	long int PRN,phoneNo;
 	string name,homeTown;
 	char gender;
 
 public:
+	// declaration of constructor with initilization of variables
 	studentInfo(){
 		rollNo = 0;
 		PRN = 0;
@@ -19,6 +23,7 @@ public:
 		gender = ' ';
 	}
 
+	// getInfo function to input data
 	void getInfo(){
 		cout<<"\nEnter Roll Number: ";
 		cin>>rollNo;
@@ -34,17 +39,24 @@ public:
 		cin>>homeTown;
 		
 	}
-
+	
+	// displayInfo function to display data
 	void displayInfo(){
 		cout<<"\n"<<"\t"<<rollNo<<"\t"<<name<<"\t"<<gender<<"\t"<<PRN<<"\t"<<phoneNo<<"\t"<<homeTown<<endl;
 	}
 };
 int main(){
+	//variable to store number of data
 	int no;
+	
+	//number of records store in 'no' variable
 	cout<<"\nEnter Number of Student: ";
 	cin>>no;
+	
+	//array of object declaration
 	studentInfo sI[no];
-
+	
+	//get info for 'no' number of data
 	for(int i = 0; i < no; i++)
 		sI[i].getInfo();
 
